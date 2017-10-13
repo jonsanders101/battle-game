738459f35e54e00bdbb1c6d62fff9b20f537b080
Jon Sanders <jonsanders101@aol.com>
2017-10-13 14:46:04 +0100

    Add message when a player wins

--- spec/features/feature_testing_spec.rb
+++ spec/features/feature_testing_spec.rb
@@ -23,10 +23,10 @@ feature "Attacking players" do
   scenario "swaps players after each vicious blow is dealt" do
     sign_in_and_play
     click_link 'Attack'
-    expect(page).to have_content "Rory attacked Chris"
-    click_link "Return to battle...if YA DARE"
-    click_link 'Attack'
     expect(page).to have_content "Chris attacked Rory"
+    click_button "Return to battle...if YA DARE"
+    click_link 'Attack'
+    expect(page).to have_content "Rory attacked Chris"
   end
 end
 
@@ -34,11 +34,33 @@ feature "deducting points" do
   scenario 'player 2 receives damage and loses life' do
     sign_in_and_play
     click_link 'Attack'
-    expect(page).to have_content "Chris: 15 points"
-    expect(page).not_to have_content "Chris: 20 points"
-    click_link "Return to battle...if YA DARE"
+    click_button "Return to battle...if YA DARE"
+    expect(page).to have_content "Rory: 15HP"
+    expect(page).not_to have_content "Rory: 20HP"
     click_link 'Attack'
-    expect(page).to have_content "Rory: 15 points"
-    expect(page).not_to have_content "Rory: 20 points"
+    click_button "Return to battle...if YA DARE"
+    expect(page).to have_content "Chris: 15HP"
+    expect(page).not_to have_content "Chris: 20HP"
+  end
+end
+
+feature "winning the game" do
+  scenario "player 2's life reaches 0" do
+    sign_in_and_play
+    click_link 'Attack'
+    click_button "Return to battle...if YA DARE"
+    click_link 'Attack'
+    click_button "Return to battle...if YA DARE"
+    click_link 'Attack'
+    click_button "Return to battle...if YA DARE"
+    click_link 'Attack'
+    click_button "Return to battle...if YA DARE"
+    click_link 'Attack'
+    click_button "Return to battle...if YA DARE"
+    click_link 'Attack'
+    click_button "Return to battle...if YA DARE"
+    click_link 'Attack'
+    click_button "Return to battle...if YA DARE"
+    expect(page).to have_content "Rory is defeated! Chris is victorious!"
   end
 end
