b79b0bf887f8d027223dd8b38307718c25a258a0
Jon Sanders <jonsanders101@aol.com>
2017-10-13 12:57:56 +0100

    Remove comments and whitespace

--- spec/features/feature_testing_spec.rb
+++ spec/features/feature_testing_spec.rb
@@ -1,10 +1,3 @@
-# feature 'Testing' do
-#     scenario "can run the content of the page" do
-#       visit('/')
-#     expect(page).to have_content "Testing infrastructure working!"
-#   end
-# end
-
 feature "Entering players" do
   scenario 'Can enter the name of the player' do
     sign_in_and_play
@@ -20,7 +13,6 @@ feature "Entering player with 20 hit points" do
   end
 end
 
-# can keep it in same feature test
 feature "Attacking players" do
   scenario 'Can attack a player' do
     sign_in_and_play
@@ -38,13 +30,10 @@ feature "Attacking players" do
   end
 end
 
-
-
 feature "deducting points" do
   scenario 'player 2 receives damage and loses life' do
     sign_in_and_play
     click_link 'Attack'
-    # click_link 'Confirmed attack!'
     expect(page).to have_content "Chris: 15 points"
     expect(page).not_to have_content "Chris: 20 points"
     click_link "Return to battle...if YA DARE"
@@ -53,15 +42,3 @@ feature "deducting points" do
     expect(page).not_to have_content "Rory: 20 points"
   end
 end
-
-
-# feature "Player 2 takes damage" do
-#   scenario 'player 2 loses HP' do
-#     sign_in_and_play
-#     # require "launchy";save_and_open_page
-#     click_link 'Attack'
-#     click_link 'Next Go'
-#     expect(page).to have_content "Chris: 15HP"
-#     # click_link 'Your Turn Player 2: Attack'
-#   end
-# end
